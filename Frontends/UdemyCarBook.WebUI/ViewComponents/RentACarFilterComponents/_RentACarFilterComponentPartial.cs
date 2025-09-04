a6922b4cea8a256d761e3b7068d7e5a0ea410995
Toprak <toprakduru@gmail.com>
2025-09-04 16:36:06 +0300

    UdemyCarBook projesi 195. kısıma kadar ki kodlar

--- Frontends/UdemyCarBook.WebUI/ViewComponents/RentACarFilterComponents/_RentACarFilterComponentPartial.cs
+++ Frontends/UdemyCarBook.WebUI/ViewComponents/RentACarFilterComponents/_RentACarFilterComponentPartial.cs
@@ -4,9 +4,11 @@ namespace UdemyCarBook.WebUI.ViewComponents.RentACarFilterComponents
 {
     public class _RentACarFilterComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        public IViewComponentResult Invoke(string v)
         {
-           return View();
+            v = "aaaa";
+            TempData["value"] = v;
+            return View();
         }
     }
 }
